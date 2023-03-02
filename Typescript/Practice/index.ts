f1435cf318670a746562ba4831558f4a1b9093f5
iamphus <phamthanhphu35@gmail.com>
2023-03-02 16:52:42 +0700

    exericise

---
+++ Typescript/Practice/index.ts
@@ -0,0 +1,233 @@
+/*Exercise:
+
+    Given the data, define the interface "User" and use it accordingly.
+
+*/
+
+// export type User = {
+//   name: string;
+//   age: number;
+//   occupation: string;
+// };
+
+// export const users: User[] = [
+//   {
+//     name: "Max Mustermann",
+//     age: 25,
+//     occupation: "Chimney sweep",
+//   },
+//   {
+//     name: "Kate Müller",
+//     age: 23,
+//     occupation: "Astronaut",
+//   },
+// ];
+
+// export function logPerson(user: User) {
+//   console.log(` - ${user.name}, ${user.age}`);
+// }
+
+// console.log("Users:");
+// users.forEach(logPerson);
+
+
+/*
+
+Intro:
+
+    All 2 users liked the idea of the community. We should go
+    forward and introduce some order. We are in Germany after all.
+    Let's add a couple of admins.
+
+    Initially we only had users in the in-memory database. After
+    introducing Admins, we need to fix the types so that
+    everything works well together.
+
+Exercise:
+
+    Type "Person" is missing, please define it and use
+    it in persons array and logPerson function in order to fix
+    all the TS errors.
+
+*/
+
+
+// interface User {
+//     name: string;
+//     age: number;
+//     occupation: string;
+// }
+
+
+// interface Admin {
+//     name: string;
+//     age: number;
+//     role: string;
+// }
+
+
+// export type Person = User | Admin;
+
+// export const persons: Person[] = [
+//     {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//     },
+//     {
+//         name: 'Jane Doe',
+//         age: 32,
+//         role: 'Administrator'
+//     },
+//     {
+//         name: 'Kate Müller',
+//         age: 23,
+//         occupation: 'Astronaut'
+//     },
+//     {
+//         name: 'Bruce Willis',
+//         age: 64,
+//         role: 'World saver'
+//     }
+// ];
+
+// export function logPerson(user: Person) {
+//     console.log(` - ${user.name}, ${user.age}`);
+// }
+
+// persons.forEach(logPerson);
+
+/*
+
+Intro:
+
+    Since we already have some of the additional
+    information about our users, it's a good idea
+    to output it in a nice way.
+
+Exercise:
+
+    Fix type errors in logPerson function.
+
+    logPerson function should accept both User and Admin
+    and should output relevant information according to
+    the input: occupation for User and role for Admin.
+
+*/
+
+// interface User {
+//     name: string;
+//     age: number;
+//     occupation: string;
+// }
+
+// interface Admin {
+//     name: string;
+//     age: number;
+//     role: string;
+// }
+
+// export type Person = User | Admin;
+
+// export const persons: Person[] = [
+//     {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//     },
+//     {
+//         name: 'Jane Doe',
+//         age: 32,
+//         role: 'Administrator'
+//     },
+//     {
+//         name: 'Kate Müller',
+//         age: 23,
+//         occupation: 'Astronaut'
+//     },
+//     {
+//         name: 'Bruce Willis',
+//         age: 64,
+//         role: 'World saver'
+//     }
+// ];
+
+// export function logPerson(person: Person) {
+//     let additionalInformation: string;
+//     if ("role" in person) {
+//         additionalInformation = person.role;
+//     } else {
+//         additionalInformation = person.occupation;
+//     }
+//     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+// }
+
+// persons.forEach(logPerson);
+
+
+/*
+
+Intro:
+
+    As we introduced "type" to both User and Admin
+    it's now easier to distinguish between them.
+    Once object type checking logic was extracted
+    into separate functions isUser and isAdmin -
+    logPerson function got new type errors.
+
+Exercise:
+
+    Figure out how to help TypeScript understand types in
+    this situation and apply necessary fixes.
+
+*/
+
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+];
+
+export function isAdmin(person: Person) : person is Admin {
+    return person.type === 'admin';
+}
+
+export function isUser(person: Person) : person is User{
+    return person.type === 'user';
+}
+
+export function logPerson(person: Person) {
+    let additionalInformation: string = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
+
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
